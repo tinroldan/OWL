1837062b5ed84502ecdf200925ee5400c5efe5b8
tinroldan <tinroldandg@gmail.com>
2021-08-16 16:27:46 -0500

    initial comit
    holi

---
+++ Assets/Inputs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour
+{
+    private bool used;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        used = false;
+    }
+
+    void Update()
+    {
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Player") && !used)
+        {
+            rb2d = col.gameObject.GetComponent<Rigidbody2D>();
+
+            rb2d.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+
+            used = true;
+
+        }
+    }
+}
---
+++ Assets/PlayerM.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerM : MonoBehaviour
+{
+    [Header("Player Value")]
+    [SerializeField]
+    float speed;
+
+
+    private bool go;
+
+    Vector2 initialPos;
+    void Start()
+    {
+        go = false;
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (go)
+        {
+            transform.position += new Vector3(1, 0, 0) * (Time.deltaTime * speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("cheese"))
+        {
+            go = false;
+        }
+
+        if (col.CompareTag("fire"))
+        {
+            transform.position = initialPos;
+            go = false;
+        }
+    }
+    
+
+
+    public void GO()
+    {
+        go = true;
+    }
+}
