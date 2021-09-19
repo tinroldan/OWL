8ae2f52378125974e92207b9791db57097d1dc29
Alonso <almear01@gmail.com>
2021-09-19 12:09:56 -0500

    Button go soun 1-7

--- Assets/Scripts/AudioScripts/AudioManager.cs
+++ Assets/Scripts/AudioScripts/AudioManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource BackgroundM;
+    AudioSource ButtonSource;
+    [SerializeField] AudioClip ButtonGo;
     void Start()
     {
         BackgroundM.Play();
+        ButtonSource = GameObject.FindGameObjectWithTag("cliprec").GetComponent<AudioSource>();
     }
-
+    public void GoButton()
+    {
+        ButtonSource.PlayOneShot(ButtonGo);
+    }
 
 }
