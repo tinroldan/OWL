3b999774fda62a90d89d5682001539b8eb7f3f4a
Alonso <almear01@gmail.com>
2021-10-07 16:18:19 -0500

    Music now continues
    No se asusten si al probar un nivel no hay música de fondo.

--- Assets/Scripts/AudioScripts/AudioManager.cs
+++ Assets/Scripts/AudioScripts/AudioManager.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource BackgroundM;
     AudioSource ButtonSource;
-    [SerializeField] AudioClip ButtonGo,InputSelect,GenericButton1,GenricButton2;
+    Scene scene;
+    private bool AlreadyExist = false;
+    [SerializeField] AudioClip ButtonGo, InputSelect, GenericButton1, GenricButton2, World1, World2, World3, World4;
     void Start()
     {
         BackgroundM.Play();
         ButtonSource = GameObject.FindGameObjectWithTag("cliprec").GetComponent<AudioSource>();
+        
+        if (GameObject.FindGameObjectsWithTag("Manager").Length < 2 && AlreadyExist == false)
+        {
+            DontDestroyOnLoad(gameObject);
+            AlreadyExist = true;
+        }
     }
     public void GoButton()
     {
@@ -27,5 +36,40 @@ public class AudioManager : MonoBehaviour
     public void inputselectsound()
     {
         ButtonSource.PlayOneShot(InputSelect);
+    }
+    private void Update()
+    {
+        scene = SceneManager.GetActiveScene();
+        switch(scene.name)
+        {
+            case "1-1":
+                BackgroundM.clip = World1;
+                if (BackgroundM.isPlaying == false)
+                {
+                    BackgroundM.Play();
+                }
+                break;
+            case "2-1":
+                BackgroundM.clip = World2;
+                if (BackgroundM.isPlaying == false)
+                {
+                    BackgroundM.Play();
+                }
+                break;
+            case "3-1":
+                BackgroundM.clip = World3;
+                if (BackgroundM.isPlaying == false)
+                {
+                    BackgroundM.Play();
+                }
+                break;
+            case "4-1":
+                BackgroundM.clip = World4;
+                if (BackgroundM.isPlaying == false)
+                {
+                    BackgroundM.Play();
+                }
+                break;
+        }
     }
 }
